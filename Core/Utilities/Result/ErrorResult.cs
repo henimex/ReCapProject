e3b41abb3276d0acbecf6d7d52ae989150f81665
J.Henimex <J.Henimex@IT-01>
2021-02-12 09:43:59 +0300

    Results

---
+++ Core/Utilities/Result/ErrorResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result
+{
+    public class ErrorResult :Result
+    {
+        public ErrorResult(string message) : base(false, message)
+        {
+            //success metodu ile aynı sekilde sadece donus degerleri false olarak calısır
+        }
+
+        public ErrorResult() : base(false)
+        {
+        }
+    }
+}
---
+++ Core/Utilities/Result/SuccessResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult(string message) : base(true, message)
+        {
+            //İşlemimin sonucu olumlu ise bu class cagrılır. Class geldiginde benden tercihen mesaj ister. eger mesaj verilir ise bu alan calısarak base olan Result a bool degerini true ve 
+            //girilmiş ise mesajı dondurur.
+        }
+
+        public SuccessResult() : base(true)
+        {
+            //eger mesaj girilmemis ise burası calısır ve base e yani Resul clasına sadece true doner.
+        }
+    }
+}
