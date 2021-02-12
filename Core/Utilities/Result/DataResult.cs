25a38e5285489029394f8fa74c4b42e17d506d86
J.Henimex <J.Henimex@IT-01>
2021-02-12 09:49:08 +0300

    Result

--- Core/Utilities/Result/DataResult.cs
+++ Core/Utilities/Result/DataResult.cs
@@ -6,7 +6,7 @@ namespace Core.Utilities.Result
 {
     public class DataResult<T> : Result, IDataResult<T>
     {
-        public DataResult(T data, bool success, string message) : base(success,message)
+        public DataResult(T data, bool success, string message) : base(success, message)
         {
             Data = data;
         }
