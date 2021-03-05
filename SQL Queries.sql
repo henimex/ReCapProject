bf65d1d9cb16aad793ce956bb7e7368d41c91d81
J.Henimex <J.Henimex@IT-01>
2021-03-05 15:57:21 +0300

    SQL Queries

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -12,16 +12,6 @@
 
 ----
 
-CREATE TABLE [dbo].[Users]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] NVARCHAR(50) NULL, 
-    [LastName] NVARCHAR(50) NULL, 
-    [Email] NVARCHAR(50) NULL, 
-    [Password] NVARCHAR(50) NULL
-)
-
-
 CREATE TABLE [dbo].[Customers] (
     [Id]          INT          IDENTITY (1, 1) NOT NULL,
     [UserId]      INT          NULL,
@@ -38,5 +28,29 @@ CREATE TABLE [dbo].[Rentals]
     [CustomerId] INT NULL, 
     [RentDate] DATETIME NULL, 
     [ReturnDate] DATETIME NULL
+)
+
+CREATE TABLE [dbo].[Users]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(50) NOT NULL, 
+    [PasswordHash] VARBINARY(500) NOT NULL, 
+    [PasswordSalt] VARBINARY(500) NOT NULL, 
+    [Status] BIT NOT NULL
+)
+
+CREATE TABLE [dbo].[OperationClaims]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(250) NOT NULL
+)
+
+CREATE TABLE [dbo].[UserOperationClaims]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [OperationClaimId] INT NOT NULL
 )
 
